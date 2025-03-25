e2dd19f7de03416ef12f3444c62a02bb3ba7c2b9
Hanseeka-Dhingana <hanseeka.bscskdkf22@iba-suk.edu.pk>
2025-03-25 22:27:05 +0500

    initial commit

---
+++ Final_practical/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Final_practical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.Press = new System.Windows.Forms.Button();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(354, 109);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(172, 26);
+            this.tb1.TabIndex = 1;
+            // 
+            // Press
+            // 
+            this.Press.Location = new System.Drawing.Point(314, 180);
+            this.Press.Name = "Press";
+            this.Press.Size = new System.Drawing.Size(147, 57);
+            this.Press.TabIndex = 2;
+            this.Press.Text = "Press";
+            this.Press.UseVisualStyleBackColor = true;
+            this.Press.Click += new System.EventHandler(this.Press_Click);
+            // 
+            // dg1
+            // 
+            this.dg1.BackgroundColor = System.Drawing.Color.White;
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.GridColor = System.Drawing.Color.White;
+            this.dg1.Location = new System.Drawing.Point(170, 304);
+            this.dg1.Name = "dg1";
+            this.dg1.RowHeadersWidth = 62;
+            this.dg1.RowTemplate.Height = 28;
+            this.dg1.Size = new System.Drawing.Size(779, 298);
+            this.dg1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 656);
+            this.Controls.Add(this.dg1);
+            this.Controls.Add(this.Press);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Button Press;
+        private System.Windows.Forms.DataGridView dg1;
+    }
+}
+
---
+++ Final_practical/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_practical
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Press_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            int num = Int32.Parse(tb1.Text);
+
+            dt.Columns.Add("num");
+            dt.Columns.Add("Multiply");
+            dt.Columns.Add("num2");
+            dt.Columns.Add("equal");
+            dt.Columns.Add("value");
+
+
+            for(int i= 1; i<=10; i++)
+            {
+                int value = num * i;
+                dt.Rows.Add(num, " * ", i, " = ", value);
+            }
+
+            dg1.DataSource = dt;
+            
+        }
+    }
+}
